0879f37cd2240081d363e3ec606c37292ed2c4d2
Rodrigo Henriques <rsh.trabalho@gmail.com>
2021-09-18 22:55:35 -0600

    Create dart project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:nba_top_shot_insights/cli.dart' as cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cli.calculate()}!');
+}
---
+++ test/cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:nba_top_shot_insights/cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
