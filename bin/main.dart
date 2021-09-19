4640cf595b8a26aa1588db6efb198a362b45b80a
Rodrigo Henriques <rsh.trabalho@gmail.com>
2021-09-18 23:07:48 -0600

    Rollback dart project

--- bin/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:nba_top_shot_insights/cli.dart' as cli;
-
-void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
-}
--- test/cli_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:nba_top_shot_insights/cli.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
